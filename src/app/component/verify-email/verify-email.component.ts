a91436d05e94b330be2fb92e4674a88ce21dbc35
deeprajiflair <deepraj.chouhan@iflair.com>
2024-02-29 19:49:09 +0530

    deep

--- src/app/component/verify-email/verify-email.component.ts
+++ src/app/component/verify-email/verify-email.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
+import { AuthService } from 'src/app/shared/auth.service';
 
 @Component({
   selector: 'app-verify-email',
   templateUrl: './verify-email.component.html',
   styleUrls: ['./verify-email.component.css']
 })
-export class VerifyEmailComponent {
+export class VerifyEmailComponent implements OnInit{
+  auth: any;
 
+  constructor(private afs:AuthService){
+   
+  }
+  ngOnInit(): void {
+    
+  }
 }
+
